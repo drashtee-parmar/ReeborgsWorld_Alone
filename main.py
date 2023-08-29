76bf850c20f91e3f41f8e686db5130a352f5ce53
Drashtee Parmar <drashteeparmar@gmail.com>
2023-08-28 22:15:51 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+def turn_around():
+    turn_left()
+    turn_left()
+    
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+#def move_up():
+    
+#turn_around()
+#move()
+#move()
+
+# Draw Square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
+move()
+
+
+
+
+
+
